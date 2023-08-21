45adb4a147c987750697b742e963860c5e023cce
hhb0 <131655783+hhb0@users.noreply.github.com>
2023-08-21 09:37:42 +0900

    Time: 39 ms (77.27%), Space: 16.4 MB (34.48%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        dp = [1]
+        for i in range(1, rowIndex + 1):
+            new_dp = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    new_dp.append(1)
+                else:
+                    new_dp.append(dp[j-1] + dp[j])
+            dp = new_dp
+        return dp
